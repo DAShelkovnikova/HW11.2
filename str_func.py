0e5684f3573af01bc862beb57157b5c45bac77b6
Darya Shelkovnikova <shelkovnikova.da@gmail.com>
2024-05-15 15:44:53 +0300

    title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def big_letter(word):
     return word.upper()
+
+
+def title_letter(string):
+    return string.title()
+"""функциz, которая делает заглавными первые буквы каждого слова в строке"""
